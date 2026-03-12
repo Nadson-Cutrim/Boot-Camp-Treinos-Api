859dec5098adf4d71de2f0a5ab3fff49d1d82b3b
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2026-03-12 00:26:01 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
-console.log("Hello, World!");
-const nome = "João";
+import "dotenv/config";
+
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+// CommonJs
+fastify.get("/", async () => {
+  return { hello: "world" };
+});
+
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: Number(process.env.PORT) || 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
