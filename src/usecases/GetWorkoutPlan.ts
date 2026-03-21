0bdb0ef8271d09c50413e6430a163c436803f91b
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2026-03-21 18:03:17 -0300

    feat: implementar a classe GetWorkoutPlan para obter detalhes de um plano de treino

---
+++ src/usecases/GetWorkoutPlan.ts
@@ -0,0 +1,61 @@
+import { ForbiddenError, NotFoundError } from "../errors/index.js";
+import { WeekDay } from "../generated/prisma/enums.js";
+import { prisma } from "../lib/db.js";
+
+interface InputDto {
+  userId: string;
+  workoutPlanId: string;
+}
+
+interface WorkoutDayDetail {
+  id: string;
+  weekDay: WeekDay;
+  name: string;
+  isRest: boolean;
+  coverImageUrl?: string;
+  estimatedDurationInSeconds: number;
+  exercisesCount: number;
+}
+
+interface OutputDto {
+  id: string;
+  name: string;
+  workoutDays: WorkoutDayDetail[];
+}
+
+export class GetWorkoutPlan {
+  async execute(dto: InputDto): Promise<OutputDto> {
+    const workoutPlan = await prisma.workoutPlan.findUnique({
+      where: { id: dto.workoutPlanId },
+      include: {
+        workoutDays: {
+          include: {
+            exercises: true,
+          },
+        },
+      },
+    });
+
+    if (!workoutPlan) {
+      throw new NotFoundError("Workout plan not found");
+    }
+
+    if (workoutPlan.userId !== dto.userId) {
+      throw new ForbiddenError("You are not the owner of this workout plan");
+    }
+
+    return {
+      id: workoutPlan.id,
+      name: workoutPlan.name,
+      workoutDays: workoutPlan.workoutDays.map((day) => ({
+        id: day.id,
+        weekDay: day.weekDay,
+        name: day.name,
+        isRest: day.isRest,
+        coverImageUrl: day.coverImageUrl ?? undefined,
+        estimatedDurationInSeconds: day.estimatedDurationInSeconds,
+        exercisesCount: day.exercises.length,
+      })),
+    };
+  }
+}
