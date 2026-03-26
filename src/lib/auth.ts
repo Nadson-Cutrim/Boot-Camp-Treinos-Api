8bea0aca1336437d8f1cfa71c9e6343c9e00834d
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2026-03-25 21:11:38 -0300

    feat: adicionar suporte a variáveis de ambiente para autenticação Google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,5 @@
+import "dotenv/config";
+
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { openAPI } from "better-auth/plugins";
@@ -7,12 +9,12 @@ import { prisma } from "./db.js";
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
   trustedOrigins: ["http://localhost:3000"],
- socialProviders:{
+  socialProviders:{
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
- },
+  },
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
